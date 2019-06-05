2c74e4daadad72524bf4371251068496dcd3b5cc
Gmantiqueira <gmantiqueira@outlook.com>
2019-06-05 00:00:30 -0300

    feature WIP | Estruturando conteúdo do Card

---
+++ src/components/CardContent/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import { ContentWrapper } from "./styles";
+
+const CardContent = ({ ticket }) => (
+  <ContentWrapper>
+    <h1>Conteúdo</h1>
+  </ContentWrapper>
+);
+
+export default CardContent;
---
+++ src/components/CardContent/styles.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const ContentWrapper = styled.div`
+  border: 1px solid #dde1e8;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+
+  height: 0;
+  padding: 24px;
+  margin: 8px auto;
+  max-width: 800px;
+  width: 100%;
+`;
