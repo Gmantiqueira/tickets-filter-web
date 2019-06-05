d9649b0ff485db89bb20ea695fa6353b25555f40
Gmantiqueira <gmantiqueira@outlook.com>
2019-06-05 19:15:01 -0300

    feature | Estruturando Card

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -2,42 +2,65 @@ import React, { Component } from "react";
 
 import { CardWrapper } from "./styles";
 
-const Card = ({ ticket }) => (
-  <CardWrapper>
-    <header>
-      <div className="last">
-        <div class="expert">
-          <h4>{ticket.Interactions[ticket.Interactions.length - 1].Sender}</h4>
-        </div>
+class Card extends Component {
+  state = {
+    isOpened: false
+  };
 
-        <div className="interaction">
-          <h4>respondeu em </h4>
-        </div>
-      </div>
+  openCard = () => {
+    if (this.state.isOpened) {
+      this.setState({ isOpened: false });
+    } else {
+      this.setState({ isOpened: true });
+    }
+  };
 
-      <div className="datecreated">
-        <h4>Criado em {ticket.DateCreate}</h4>
-      </div>
-    </header>
+  render() {
+    return (
+      <CardWrapper onClick={this.openCard} isOpened={this.state.isOpened}>
+        <header>
+          <div className="last">
+            <div class="expert">
+              <h4>
+                {
+                  this.props.ticket.Interactions[
+                    this.props.ticket.Interactions.length - 1
+                  ].Sender
+                }
+              </h4>
+            </div>
 
-    <main>
-      <div class="message">
-        <h4>
-          {ticket.Interactions[
-            ticket.Interactions.length - 1
-          ].Message.substring(0, 70)}{" "}
-          {ticket.Interactions[ticket.Interactions.length - 1].Message.length >
-          40
-            ? "..."
-            : ""}
-        </h4>
-      </div>
+            <div className="interaction">
+              <p>respondeu em </p>
+            </div>
+          </div>
 
-      <div class="number">
-        <h4>{ticket.Interactions.length} interações</h4>
-      </div>
-    </main>
-  </CardWrapper>
-);
+          <div className="datecreated">
+            <h4>Criado em {this.props.ticket.DateCreate}</h4>
+          </div>
+        </header>
+
+        <main>
+          <div class="message">
+            <h4>
+              {this.props.ticket.Interactions[
+                this.props.ticket.Interactions.length - 1
+              ].Message.substring(0, 70)}{" "}
+              {this.props.ticket.Interactions[
+                this.props.ticket.Interactions.length - 1
+              ].Message.length > 40
+                ? "..."
+                : ""}
+            </h4>
+          </div>
+
+          <div class="number">
+            <h4>{this.props.ticket.Interactions.length} interações</h4>
+          </div>
+        </main>
+      </CardWrapper>
+    );
+  }
+}
 
 export default Card;
--- src/components/Card/styles.js
+++ src/components/Card/styles.js
@@ -2,14 +2,16 @@ import styled from "styled-components";
 
 export const CardWrapper = styled.div`
   border: 1px solid #dde1e8;
+  cursor: pointer;
   display: flex;
   flex-direction: column;
   justify-content: space-between;
 
-  height: 116px;
+  height: ${props => (props.isOpened ? `360px` : `116px`)};
   padding: 24px;
   margin: 8px auto;
   max-width: 800px;
+  transition: 0.5s ease all;
   width: 100%;
 
   header,
@@ -23,7 +25,15 @@ export const CardWrapper = styled.div`
       display: flex;
       flex-wrap: wrap;
       .expert {
+        font-size: 14px;
+        font-weight: bold;
         margin-right: 8px;
+      }
+      .interaction {
+        color: #94a0b6;
+        font-size: 14px;
+        font-weight: normal;
+        line-height: 142%;
       }
     }
   }
