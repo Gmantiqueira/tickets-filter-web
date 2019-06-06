2d5669b26edb90f8fed8d733c25de84bfa234cd9
Gmantiqueira <gmantiqueira@outlook.com>
2019-06-05 23:53:32 -0300

    feature | Fontes e Assets

--- src/styles/global.js
+++ src/styles/global.js
@@ -2,7 +2,7 @@ import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
     * {
-        font-family: 'Work Sans', sans-serif;
+        font-family: 'Nunito', sans-serif;
         margin: 0;
         padding: 0;
         box-sizing: border-box;
@@ -13,6 +13,38 @@ const GlobalStyle = createGlobalStyle`
 
     html, body, #root {
         height: 100%;
+    }
+    h4{
+        font-size: 14px;
+    }
+
+    p{
+        font-size: 12px;
+    }
+
+    .darker{
+        color: #414C61;
+    }
+    .dark{
+        color: #54627C;
+    }
+    .normal{
+        color: #677897;
+    }
+    .light{
+        color: #94A0B6;
+    }
+
+    .regular{
+        font-weight: normal;
+    }
+
+    .semibold{
+        font-weight: 600;
+    }
+    
+    .bold{
+        font-weight: bold;
     }
 
     .relative{
