40aaedaaeade3cff05880bc11a48473c2d99bfd7
unknown <gmantiqueira@gmail.com>
2019-06-07 11:16:45 -0300

    bugfix | Fix

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,18 +1,18 @@
-import React, { Component, Fragment } from "react";
+import React, { Component } from "react";
 
 import TicketList from "../../components/TicketList";
 import Filter from "../../components/Filter";
 import { Container } from "./styles";
 
 class Home extends Component {
-  render() {
-    return (
-      <Container>
-        <Filter />
-        <TicketList />
-      </Container>
-    );
-  }
+    render() {
+        return ( 
+          <Container>
+            <Filter/>
+            <TicketList/>
+          </Container>
+        );
+    }
 }
 
 export default Home;
