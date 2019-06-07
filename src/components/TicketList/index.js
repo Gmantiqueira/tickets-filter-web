59ec5f984c68bfd1b898fe5321373480c9545661
Gmantiqueira <gmantiqueira@outlook.com>
2019-06-07 00:24:46 -0300

    feature | Reestruturando API

--- src/components/TicketList/index.js
+++ src/components/TicketList/index.js
@@ -1,15 +1,63 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
+import api from "../../services/api";
 
 import { Container } from "./styles";
 import Card from "../Card";
 
-const TicketList = ({ tickets }) => (
-  <Container>
-    {tickets.map(ticket => (
-      <Card ticket={ticket} />
-    ))}
-  </Container>
-);
+class TicketList extends Component {
+  state = {
+    tickets: [],
+    url: "/?page=1",
+    page: 1
+  };
+
+  apiGet = async e => {
+    try {
+      const { data: tickets } = await api.get(this.state.url);
+
+      this.setState({
+        ...this.state.tickets,
+        tickets
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  componentDidMount() {
+    this.apiGet();
+    window.addEventListener("scroll", this.ajaxPagination);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("scroll", this.ajaxPagination);
+  }
+
+  ajaxPagination = async e => {
+    if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
+      this.setState({ page: this.state.page + 1 });
+      this.setState({
+        url: "/?page=" + this.state.page
+      });
+      const { data: tickets } = await api.get(this.state.url);
+
+      this.setState({
+        ...this.state.tickets,
+        tickets
+      });
+    }
+  };
+
+  render() {
+    return (
+      <Container>
+        {this.state.tickets.map(ticket => (
+          <Card ticket={ticket} />
+        ))}
+      </Container>
+    );
+  }
+}
 
 export default TicketList;
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,39 +1,16 @@
 import React, { Component, Fragment } from "react";
-import api from "../../services/api";
 
 import TicketList from "../../components/TicketList";
 import Filter from "../../components/Filter";
 import { Container } from "./styles";
 
 class Home extends Component {
-  state = {
-    tickets: [],
-    url: "/?orderby=score_reverse"
-  };
-
-  apiGet = async e => {
-    try {
-      const { data: tickets } = await api.get(this.state.url);
-
-      console.log(tickets);
-      this.setState({
-        tickets: tickets
-      });
-    } catch (err) {
-      console.log(err);
-    }
-  };
-
-  componentDidMount() {
-    this.apiGet();
-  }
-
   render() {
     return (
-      <Fragment>
-        <Filter apiUrl={this.state.url} />
-        <TicketList tickets={this.state.tickets} />
-      </Fragment>
+      <Container>
+        <Filter />
+        <TicketList />
+      </Container>
     );
   }
 }
