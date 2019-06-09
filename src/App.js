622bd4d5b161071748400fa8038b53ec8c98da02
Gmantiqueira <gmantiqueira@outlook.com>
2019-06-09 01:12:17 -0300

    bugfix | Retirando rotas

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import React, { Component } from "react";
 import { Provider } from "react-redux";
-import { BrowserRouter } from "react-router-dom";
 
+import Home from "./pages/home";
 import GlobalStyle from "./styles/global";
-
 import { Container } from "./styles/components";
-
 import store from "./store";
-import Routes from "./routes";
 
 class App extends Component {
   render() {
     return (
       <Provider store={store}>
-        <BrowserRouter>
-          <GlobalStyle />
-          <Container>
-            <Routes />
-          </Container>
-        </BrowserRouter>
+        <GlobalStyle />
+        <Container>
+          <Home />
+        </Container>
       </Provider>
     );
   }
